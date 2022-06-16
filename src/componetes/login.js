2ac03b9952c96cdee4e75e5872ac0210adda49e5
pedro Vinicius <pedro.carvalho161@gmail.com>
2022-06-16 03:51:45 -0300

    Update login.js
    sao 4 da manha e eu nao consegue evoluir muito

--- src/componetes/login.js
+++ src/componetes/login.js
@@ -5,7 +5,23 @@ const login =()=>{
     return(
         <div className="user">
             <h1 > login</h1>
-            <div></div>
+            <div className="acessarApp"> <hi>acessar Site</hi>
+            </div>
+               <div className="loginInputemail">
+                <input
+                type="text"
+                 placeholder="Digite o seu Email"
+                 />
+                 <div className="senhaInput">
+                    <input
+                    type="text"
+                    placeholder="Digite o sua senha"
+                    />
+
+                 </div>
+
+             </div> 
+
              </div>
              
     ) 
