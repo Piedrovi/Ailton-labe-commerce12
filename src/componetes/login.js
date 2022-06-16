0da125806b816e9349af15109bc3071a7ea274bc
pedro Vinicius <pedro.carvalho161@gmail.com>
2022-06-16 03:49:42 -0300

    chuva muit forte, so consegui fazer um esqueleto do login sao 03:49, infelizmente a chuva vem me atraplando com muita frequencia

---
+++ src/componetes/login.js
@@ -0,0 +1,14 @@
+import React from "react"
+import'./login.css'
+
+const login =()=>{
+    return(
+        <div className="user">
+            <h1 > login</h1>
+            <div></div>
+             </div>
+             
+    ) 
+}
+
+export default login
